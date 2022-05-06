763854b3bad3ec8fa5104077584163c9adf3de32
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-05-05 20:28:31 -0500

    Requisiçao post criada e funcionando, testada com front end

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import chalk from "chalk";
 import cors from "cors";
@@ -7,23 +7,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const Port = process.env.PORT;
+const Database = process.env.DATABASE;
 const MongoURL = process.env.MONGO_URL;
 
 const app = express();
 app.use(cors());
 app.use(json());
 
+// CRIAR UM ARQUIVO SEPARADO PARA CONECTAR COM O BANCO DE DADOS
+
 let db = null;
 const mongoClient = new MongoClient(MongoURL);
 try {
     await mongoClient.connect();
-    db = mongoClient.db("test");
+    db = mongoClient.db(Database);
     console.log(chalk.bold.green("Banco de dados conectado, show!!"))
 }
 catch (e) {
     console.log(e);
     // res.status(500).send(chalk.red.bold("Falha na conexão com o banco"))
 }
+
+app.post("/cadastro", async (req, res) => {
+    const { nome, email, senha, confirmação } = req.body;
+    console.log(nome, email, senha, confirmação);
+    res.sendStatus(201);
+}
+)
 
 app.listen(Port, () => {
     console.log(chalk.bold.blue(`Servidor conectado na porta ${Port}`))
